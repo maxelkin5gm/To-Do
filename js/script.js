cbecbccc12d1c269fb95ebb0379a9b0c00f32082
maxelkin5gm <68002377+maxelkin5gm@users.noreply.github.com>
2021-02-20 21:46:05 +0300

    update sort

--- js/script.js
+++ js/script.js
@@ -1,20 +1,21 @@
 "use strict"
 
-
+let data = JSON.parse(localStorage.getItem('data'));
+if (data == null) { data = {}; }
+console.log(data);
 let buttonADD = document.querySelector('#btn');
 let ul = document.querySelector('#do');
 let input = document.querySelector('#input');
 
 
-function getStorage() {
-	let keys = Object.keys(localStorage);
-	for (let key of keys) {
-		var i = 1;
+function createElement(inputDATA, ul) {
+	if (inputDATA) {
+		let dataInput = inputDATA;
 		let li = document.createElement('li');
 		let div = document.createElement('div');
 		let span = document.createElement('span');
 
-		div.textContent = localStorage.getItem(key);
+		div.textContent = inputDATA;
 		div.className = 'text';
 
 		span.innerHTML = '&times;'
@@ -26,47 +27,9 @@ function getStorage() {
 		li.style.opacity = '0';
 		setTimeout(function () { li.style.opacity = '1'; }, 0);
 
-		li.onclick = function () {
-			this.firstElementChild.classList.toggle('checked');
-			li.classList.toggle('checked');
-		}
-
-		span.onclick = function () {
-			li.style.opacity = '1';
-			li.style.opacity = '0';
-			setTimeout(function () { span.parentNode.remove(); }, 200);
-			localStorage.removeItem(key);
-		};
-		i++;
-	}
-	return i;
-}
-
-let i = getStorage();
-
-
-
-buttonADD.onclick = function () {
-	if (input.value) {
-		let dataInput = input.value;
-		let li = document.createElement('li');
-		let div = document.createElement('div');
-		let span = document.createElement('span');
-
-		div.textContent = input.value
-		div.className = 'text';
-
-		span.innerHTML = '&times;'
-		span.className = 'close';
-
-		li.append(div, span);
-
-		ul.append(li);
-		li.style.opacity = '0';
-		setTimeout(function () { li.style.opacity = '1'; }, 0);
-
-		localStorage.setItem(input.value, input.value);
-		i++;
+		// localStorage.setItem(String(inputDATA), String(inputDATA));
+		data[inputDATA] = inputDATA;
+		localStorage.setItem('data', JSON.stringify(data));
 
 		li.onclick = function () {
 			this.firstElementChild.classList.toggle('checked');
@@ -77,9 +40,22 @@ buttonADD.onclick = function () {
 			li.style.opacity = '1';
 			li.style.opacity = '0';
 			setTimeout(function () { span.parentNode.remove(); }, 300);
-			localStorage.removeItem(dataInput);
+			delete data[dataInput];
+			if (Object.keys(data).length == 0) {
+				localStorage.removeItem('data');
+			} else
+				localStorage.setItem('data', JSON.stringify(data));
 		};
-
-		input.value = '';
 	}
+};
+
+
+for (let key in data) {
+	createElement(key, ul);
+};
+
+buttonADD.onclick = function () {
+	createElement(input.value, ul);
+	console.log(Object.keys(localStorage));
+	input.value = '';
 };
